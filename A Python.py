3c8bdec8e88f1ad6164b1c63ab96fb0209863e3d
swetapal2122002-eng <swetapal2122002@gmail.com>
2025-11-13 18:23:16 +0530

    Add files via upload

---
+++ A Python.py
@@ -0,0 +1,101 @@
+
+a = float(input("enter first no."))
+b = float(input("enter second no."))
+op = input("enter the operator(+,-,*,/):")
+if op == "+":
+    print("result:",a+b)
+elif op == "-":
+    print("result:",a-b)
+elif op == "*":
+    print("result:",a*c)
+elif op == "/":
+    if b!= 0:
+        print("result:",a/b)
+    else:print("cannot divide by zero:")
+else:print("invalid operator")
+
+name = input("enter your name: ")
+age = input("enter your age: ")
+ocupation = input("enter your ocupation: ")
+city = input("enter your city: ")
+print("Hello," , name)
+print("age:" , age)
+print("ocupation:" ,ocupation)
+print("city:" ,city)
+
+while True:
+    user_input = input("Enter a command (or type 'exit' to stop): ").lower()
+    if user_input == 'exit':
+        print("Exiting the program. ")
+        break
+    else:
+        print(f"You entered: {user_input} ")
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
