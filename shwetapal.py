7d792907d30e4d8106c3f9aa0fa2aac1560a2da4
swetapal2122002-eng <swetapal2122002@gmail.com>
2025-11-14 21:34:56 +0530

    Add files via upload

---
+++ shwetapal.py
@@ -0,0 +1,18 @@
+# task lists
+
+m = [1,2,3,4,5]
+indexed=m.index(3)
+print("posion of the element", indexed)
+
+My_list = [1,2,3,4,5,6,7,8,9,10]
+print(My_list[2:6])
+
+student_name = ["Abdul","Vipul","Sahil","Sonali","vani"]
+print("initial list:")
+print(student_name)
+student_name.append("Shajid")
+print(student_name)
+student_name.remove("Abdul")
+print(student_name)
+print("after removing Abdul")
+print(student_name)
